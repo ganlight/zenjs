292700dc0fccabed3d73a3869b91056394eb5ced
ganlight <ganlight@foxmail.com>
2016-11-25 16:01:22 +0800

    markdown blog

--- tmp/zen/zen.js
+++ tmp/zen/zen.js
@@ -266,11 +266,21 @@ var Filter = {
 
 var MarkDown = {
     converter: null,
-    init: function() {
+    init: function(articles) {
         if (showdown && showdown.Converter) {
             this.converter = new showdown.Converter();
         } else {
             $(".title").html("暂不支持markdown");
+        }
+        Catalog.init(articles);
+        var type = Util.getPar("type");
+        var file = Util.getPar("file");
+        var article = Catalog.get(type, file);
+        if (article) {
+            MarkDown.load(article);
+        } else {
+            $(".markdown-area").hide();
+            $(".catalog-area").show();
         }
     },
     rend: function(article, data) {
@@ -289,12 +299,57 @@ var MarkDown = {
             async: false,
             dataType: 'html',
             success: function(data) {
+                $(".catalog-area").hide();
+                $(".markdown-area").show();
                 self.rend(article, data);
             },
             error: function(e) {
-                $(".markdown-area").text("当前文章" + article.file + "不存在，请返回其他文章");
+                $(".markdown-area").hide();
+                $(".catalog-area").show();
+                Message.toast("当前文章" + article.file + "不存在，请返回其他文章");
             }
         });
+    }
+}
+
+var Catalog = {
+    articles: [],
+    types: null,
+    sections: null,
+    map: {},
+    init: function(articles) {
+        this.articles = articles || [];
+        this.rend();
+    },
+    rend: function() {
+        var self = this;
+        if (this.articles && this.articles.length > 0) {
+            var len = this.articles.length;
+            var data = this.articles;
+            var parent = $(".catalog-area");
+            for (var i = 0; i < data.length; i++) {
+                var item = data[i];
+                var clone = $(".page-template .list-item").clone();
+                Store.data(clone, item);
+                self.map[item.type + "/" + item.file] = item;
+                Util.rendValue(clone, item);
+                self.bind(clone);
+                parent.append(clone);
+            }
+        }
+    },
+    get: function(type, file) {
+        var item = this.map[type + "/" + file] || "";
+        return item;
+    },
+    bind: function(clone) {
+        var self = this;
+        clone.click(function() {
+            var data = Store.data(clone);
+            if (data && data.file) {
+                window.location.href = "#" + Util.getHash() + "?type=" + data.type + "&file=" + data.file;
+            }
+        })
     }
 }
 
