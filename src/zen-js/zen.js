3288335972ccf28a76fabdcffaaf84d8247b5a32
ganlight <ganlight@foxmail.com>
2016-11-25 10:40:18 +0800

    cdn showdown fastclick font-awesome

--- src/zen-js/zen.js
+++ src/zen-js/zen.js
@@ -1,6 +1,7 @@
 var Zen = {
     mode: "normal",
     boot: function() {
+        this.support();
         $(document).ready(function() {
             Zen.init();
             Zen.load();
@@ -23,6 +24,41 @@ var Zen = {
         }
         Store.sLocal("ZEN_DEBUG", mode);
         this.isDebug = mode;
+    },
+    support: function() {
+        //用于加载其他cdn库，zepto必须先引入，只引入其他的need的选项
+        var libs = {
+            "zepto": {
+                type: "script",
+                import: "must",
+                cdn: "<script src='//cdn.bootcss.com/zepto/1.2.0/zepto.min.js'></script>"
+            },
+            "fastclick": {
+                type: "script",
+                import: "need",
+                cdn: "<script src='//cdn.bootcss.com/fastclick/1.0.6/fastclick.min.js'></script>"
+            },
+            "showdown": {
+                type: "script",
+                import: "need",
+                cdn: "<script src='//cdn.bootcss.com/showdown/1.5.0/showdown.min.js'></script>"
+            },
+            "font-awesome": {
+                type: "style",
+                import: "need",
+                cdn: "<link href='//cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>"
+            }
+        }
+        for (var lib in libs) {
+            if (libs[lib].import == "need") {
+                var ele = $(libs[lib]["cdn"]);
+                if (libs[lib].type == "style") {
+                    $("title").after(ele);
+                } else {
+                    $("head").append(ele);
+                }
+            }
+        }
     },
     init: function() {
         this.isDebug = Store.gLocal("ZEN_DEBUG") || false;
