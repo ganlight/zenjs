ff967aacd3fc8d03f431e1495c5aeb8c8f05c4a8
ganlight <ganlight@foxmail.com>
2016-11-28 11:12:23 +0800

    encrypt

--- dist/lib/zenjs.min.js
+++ dist/lib/zenjs.min.js
@@ -161,6 +161,123 @@ var Check = {
     }
 }
 
+/**
+*Encrypt 原理参照 字符串的加密解密,还有uft8的转化
+http://www.codefans.net/jscss/code/3406.shtml
+http://files.cnblogs.com/files/mofish/base64.js
+*/
+var Encrypt = {
+    encode: function(str, pwd) {
+        if (pwd == null || pwd.length <= 0) {
+            pwd = "zenjs";
+        }
+        str = this.utf8_encode(str);
+        var prand = "";
+        for (var i = 0; i < pwd.length; i++) {
+            prand += pwd.charCodeAt(i).toString();
+        }
+        var sPos = Math.floor(prand.length / 5);
+        var mult = parseInt(prand.charAt(sPos) + prand.charAt(sPos * 2) + prand.charAt(sPos * 3) + prand.charAt(sPos * 4) + prand.charAt(sPos * 5));
+        var incr = Math.ceil(pwd.length / 2);
+        var modu = Math.pow(2, 31) - 1;
+        if (mult < 2) {
+            return null;
+        }
+        var salt = Math.round(Math.random() * 1000000000) % 100000000;
+        prand += salt;
+        while (prand.length > 10) {
+            prand = (parseInt(prand.substring(0, 10)) + parseInt(prand.substring(10, prand.length))).toString();
+        }
+        prand = (mult * prand + incr) % modu;
+        var enc_chr = "";
+        var enc_str = "";
+        for (var i = 0; i < str.length; i++) {
+            enc_chr = parseInt(str.charCodeAt(i) ^ Math.floor((prand / modu) * 255));
+            if (enc_chr < 16) {
+                enc_str += "0" + enc_chr.toString(16);
+            } else enc_str += enc_chr.toString(16);
+            prand = (mult * prand + incr) % modu;
+        }
+        salt = salt.toString(16);
+        while (salt.length < 8) salt = "0" + salt;
+        enc_str += salt;
+        return enc_str;
+    },
+    decode: function(str, pwd) {
+        if (str == null || str.length < 8) {
+            return;
+        }
+        if (pwd == null || pwd.length <= 0) {
+            pwd = "zenjs";
+        }
+        var prand = "";
+        for (var i = 0; i < pwd.length; i++) {
+            prand += pwd.charCodeAt(i).toString();
+        }
+        var sPos = Math.floor(prand.length / 5);
+        var mult = parseInt(prand.charAt(sPos) + prand.charAt(sPos * 2) + prand.charAt(sPos * 3) + prand.charAt(sPos * 4) + prand.charAt(sPos * 5));
+        var incr = Math.round(pwd.length / 2);
+        var modu = Math.pow(2, 31) - 1;
+        var salt = parseInt(str.substring(str.length - 8, str.length), 16);
+        str = str.substring(0, str.length - 8);
+        prand += salt;
+        while (prand.length > 10) {
+            prand = (parseInt(prand.substring(0, 10)) + parseInt(prand.substring(10, prand.length))).toString();
+        }
+        prand = (mult * prand + incr) % modu;
+        var enc_chr = "";
+        var enc_str = "";
+        for (var i = 0; i < str.length; i += 2) {
+            enc_chr = parseInt(parseInt(str.substring(i, i + 2), 16) ^ Math.floor((prand / modu) * 255));
+            enc_str += String.fromCharCode(enc_chr);
+            prand = (mult * prand + incr) % modu;
+        }
+        enc_str = this.utf8_decode(enc_str);
+        return enc_str;
+    },
+    utf8_encode: function(string) {
+        string = string.replace(/\r\n/g, "\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            } else if ((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            } else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+        return utftext;
+    },
+    utf8_decode: function(utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+        while (i < utftext.length) {
+            c = utftext.charCodeAt(i);
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            } else if ((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i + 1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            } else {
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        }
+        return string;
+    }
+}
+
 var Filter = {
     money: function(value) {
         if (value == "" || isNaN(value) || value == Infinity) {
