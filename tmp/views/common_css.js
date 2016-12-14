b77fa79be4dd3018f008acb1cd30231507b73827
ganlight <ganlight@foxmail.com>
2016-12-14 18:29:08 +0800

    zen-stack

--- tmp/views/common_css.js
+++ tmp/views/common_css.js
@@ -1,8 +1,6 @@
-views.common_css = function() {/*<style>.zen-container {
+views.common_css = function() {/*<style>.zen-stack {
     background: url("assets/img/background.jpg");
     background-repeat: no-repeat;
     background-size: 100% 100%;
-    top:0;
-    left: 0;
 }
 </style>*/}
