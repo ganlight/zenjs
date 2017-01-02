2e01545283746a0513784bef2dcf24ed1cc171ab
ganlight <ganlight@foxmail.com>
2017-01-02 21:04:35 +0800

    parse

--- zen/js/parse.js
+++ zen/js/parse.js
@@ -1,3 +1,8 @@
+zen.parse_decode = function(str) {
+    str.replace(/__block_head__/g, '/*').replace(/__block_foot__/g, "*\/");
+    return str;
+}
+
 zen.parse = function(fn) {
     //这里面主要将html和js代码转化成js函数，通过这样的方式，可以获取里面的内容
     //如果是多行文本采用下面的方式
@@ -8,7 +13,6 @@ zen.parse = function(fn) {
     }
     if (typeof fn === 'function') {
         var string = fn.toString();
-        string = decode(string);
         if (string.length > 20) {
             return string.slice(15, -3);
         }
@@ -24,5 +28,5 @@ zen.content = function(path) {
     }
     var script_name = 'Zen.views["' + path + '"]';
     content = zen.parse(eval(script_name));
-    return content;
+    return content.replace(/__block_head__/g, '/*').replace(/__block_foot__/g, "*\/");
 }
