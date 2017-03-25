90a364d22f3bbfbacc4f29d255f5a0109a079f22
ganlight <ganlight@foxmail.com>
2017-03-25 23:31:09 +0800

    design page

---
+++ src/views/design/config.js
@@ -0,0 +1,13 @@
+var DESIGN_DIC = [{
+    title: "ZENJS个人网站设计",
+    desc: "ZENJS个人网站设计",
+    public: "2016-11-25 14:00",
+    auther: "ganlight",
+    file: "zenjs/zenjs-design"
+}, {
+    title: "ZENJS个人网站设计-Logo设计",
+    desc: "ZENJS个人网站设计-Logo设计",
+    public: "2016-11-25 14:00",
+    auther: "ganlight",
+    file: "zenjs/zenjs-design"
+}]
